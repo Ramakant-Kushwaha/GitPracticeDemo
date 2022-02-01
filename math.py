014ea954f745d4d4aa5fceff54c3cab8d486160a
Ramakant Kushwaha <ramakantkushwaha1995@gmail.com>
2022-02-01 11:30:44 +0530

    ADDED Function Header

--- math.py
+++ math.py
@@ -1,8 +1,8 @@
 def add(x,y): #add implimentation
     pass
-def subtract(x,y):
+def subtract(x,y):  #sub implimentation
     pass
-def multiply(x,y):
+def multiply(x,y):  #mul implimentation
     pass
-def divide(x,y):
+def divide(x,y):    #div implimentation
     pass
